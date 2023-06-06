d6a025f22967deb4627a192bb90a615c1e60a524
Ibrahim Uzun <info@ibrahimuzun.com>
2023-06-07 01:47:35 +0300

    init

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,13 +1,40 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 export async function GET() {
-        return NextResponse.json({
-                message: 'Here is an example of API route.',
-                obj: {
-                    name: 'Ibrahim Uzun',
-                    born: 1996,
-                    address: 'Uskudar, Istanbul, Turkey',
-                    hobbies: ['bycicle', 'hiking', 'chess', 'coding'],
-                }
-        });
+  const today = new Date()
+
+  //today: day(number) mount(string) year(number)
+  const todayString = `${today.getDate()} ${today.toLocaleString('default', { month: 'long' })} ${today.getFullYear()}`
+  return NextResponse.json({
+    city: "Istanbul",
+    country: "Turkey",
+    timezone: "Europe/Istanbul",
+    date:todayString,
+    prayer_times: [
+      {
+        name: "Imsak",
+        time: "04:21",
+      },
+      {
+        name: "Güneş",
+        time: "05:52",
+      },
+      {
+        name: "Öğle",
+        time: "13:11",
+      },
+      {
+        name: "İkindi",
+        time: "17:00",
+      },
+      {
+        name: "Akşam",
+        time: "20:19",
+      },
+      {
+        name: "Yatsı",
+        time: "21:48",
+      },
+    ],
+  });
 }
